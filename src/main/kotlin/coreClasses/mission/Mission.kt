7f8eb640ae3bf9b06a5812b396eadeb1132c8369
Lidin <nicolas.lidin@gmail.com>
2021-03-20 04:09:26 +0000

    refactor -> remove duplica missionId

--- src/main/kotlin/coreClasses/mission/Mission.kt
+++ src/main/kotlin/coreClasses/mission/Mission.kt
@@ -31,12 +31,9 @@ class Mission(
         var defaultMaxStageTime = 5000f
     }
     var startTime = Utils.getRandomNumberInRange(100f, 3000f)
-    lateinit var missionId: String
 
     override fun run() {
         try {
-            // yes this identifier is a simplification for debugging and a thread can actually be shared between multiples tasks
-            this.missionId = Thread.currentThread().name
             Utils.log(Thread.currentThread().name + " Mission starting.")
             Utils.log("---------")
             scheduleStages()
@@ -50,7 +47,7 @@ class Mission(
         if (Utils.getRandomNumberInRange(0f, 100f) <= 10f) {
             GlobalScope.launch {
                 missionNetworkService.sendMessage(
-                    messageContent = "stage transition failed of mission $missionId",
+                    messageContent = "stage transition failed",
                     newMessageType = MessageType.Failure
                 )
             }
@@ -59,11 +56,11 @@ class Mission(
                 runBlocking {
                     missionNetworkService.sendMessage(messageContent = "abort mission, update fail", newMessageType = MessageType.AbortMission)
                 }
-                throw Exception("Mission $missionId abort, update non successfull")
+                throw Exception("Mission abort, update non successfull")
             } else {
                 GlobalScope.launch {
                     missionNetworkService.sendMessage(
-                        messageContent = "Mission $missionId has sucessfully recovered from the software update",
+                        messageContent = "Mission has sucessfully recovered from the software update",
                         newMessageType = MessageType.SuccessfulSoftwareUpdate
                     )
                 }
@@ -119,21 +116,21 @@ class Mission(
             Utils.delay(transitStepTime)
             currentDistanceFromController += destinationDistanceQuartile
             System.err.println("$id 1")
-            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN Mission: $missionId $ANSI_RESET $ANSI_RED inter transit stage 1 $ANSI_RESET $ANSI_GREEN in progress: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 2)
+            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN MissionStatus: $ANSI_RESET $ANSI_RED inter transit stage 1 $ANSI_RESET $ANSI_GREEN in progress: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 2)
             degradeAndSendComponentsMessages("inter transit stage 1")
             Utils.delay(transitStepTime)
             currentDistanceFromController += destinationDistanceQuartile
             System.err.println("$id 2")
-            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN Mission: $missionId $ANSI_RESET $ANSI_RED inter transit stage 2 $ANSI_RESET $ANSI_GREEN in progress, middle of the mission: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 3)
+            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN MissionStatus: $ANSI_RESET $ANSI_RED inter transit stage 2 $ANSI_RESET $ANSI_GREEN in progress, middle of the mission: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 3)
             degradeAndSendComponentsMessages("inter transit stage 2")
             Utils.delay(transitStepTime)
             currentDistanceFromController += destinationDistanceQuartile
             System.err.println("$id 3")
-            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN Mission: $missionId $ANSI_RESET $ANSI_RED inter transit stage 3 $ANSI_RESET $ANSI_GREEN in progress: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 4)
+            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN MissionStatus: $ANSI_RESET $ANSI_RED inter transit stage 3 $ANSI_RESET $ANSI_GREEN in progress: we are at $currentDistanceFromController millions km from the earth, we are at ${destination.distance - currentDistanceFromController} millions kms from destination: ${destination.name} $ANSI_RESET", newMessageType = MessageType.InterTransitStage, distanceFromEarthQuintile = 4)
             degradeAndSendComponentsMessages("inter transit stage 3")
             Utils.delay(transitStepTime)
             currentDistanceFromController += destinationDistanceQuartile
-            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN Mission: $missionId $ANSI_RESET $ANSI_RED transit stage ended: $ANSI_RESET w$ANSI_GREEN e are at $currentDistanceFromController millions km from the earth, destination: ${destination.name} has been reached! $ANSI_RESET", newMessageType = MessageType.TransitStage, distanceFromEarthQuintile = 5)
+            missionNetworkService.sendMessage(messageContent = "$ANSI_GREEN MissionStatus: $ANSI_RESET $ANSI_RED transit stage ended: $ANSI_RESET w$ANSI_GREEN e are at $currentDistanceFromController millions km from the earth, destination: ${destination.name} has been reached! $ANSI_RESET", newMessageType = MessageType.TransitStage, distanceFromEarthQuintile = 5)
             System.err.println("-------------------END OF TRANSIT-----------------------")
         }
         this.missionNetworkService.listenIncommingMessage()
